85d3279e3c7f9b90df5c6509dc66f4734f3a272e
benjamin <benng822@gmail.com>
2019-09-18 16:52:55 +0800

    adding files for gihub pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box-component";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        users: [],
+        searchField: ""
+    };
+
+    componentDidMount() {
+        axios.get("https://jsonplaceholder.typicode.com/users").then(res => {
+            const users = res.data;
+            this.setState({ users });
+        });
+    }
+
+    onInputChange = e => {
+        this.setState({ searchField: e.target.value });
+    };
+
+    render() {
+        const { users, searchField } = this.state;
+        const filterUsers = users.filter(user => {
+            return user.name.toLowerCase().includes(searchField.toLowerCase());
+        });
+        return (
+            <div className="App">
+                <h1>Monster Rolodex</h1>
+                <SearchBox
+                    placeHolder="Search Monster"
+                    searchField={this.state.searchField}
+                    handleChange={this.onInputChange}
+                />
+                <CardList users={filterUsers} />
+            </div>
+        );
+    }
 }
 
 export default App;
