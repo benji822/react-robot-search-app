73aca35f1d1b231185852af4393494628082f13d
benjamin <benng822@gmail.com>
2019-09-18 16:51:55 +0800

    Updates

---
+++ precache-manifest.fac7f6aa74a67770cf4e6a4173afe2e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf262be9a103db35cfd7ded53f2af47",
+    "url": "/react-robot-search-app/index.html"
+  },
+  {
+    "revision": "ac46d4b3053018d84a33",
+    "url": "/react-robot-search-app/static/css/main.66ebe81d.chunk.css"
+  },
+  {
+    "revision": "44d46dc4af5f9e0f015a",
+    "url": "/react-robot-search-app/static/js/2.6ee05573.chunk.js"
+  },
+  {
+    "revision": "ac46d4b3053018d84a33",
+    "url": "/react-robot-search-app/static/js/main.b5d4950f.chunk.js"
+  },
+  {
+    "revision": "8ddd509b653ef5bc9bcf",
+    "url": "/react-robot-search-app/static/js/runtime~main.f5b809cd.js"
+  }
+]);
